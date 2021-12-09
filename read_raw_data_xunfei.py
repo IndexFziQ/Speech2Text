095be2fe0acc2e2885e36d18e932776c40155b50
IndexFziQ <IndexFziQ@gmail.com>
2021-12-09 17:59:29 +0800

    first commit

---
+++ read_raw_data_xunfei.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+# author: xieyuqiang
+# email: xieyuqiang@iie.ac.cn
+# 2021.11.30
+# read raw_data from Xun Fei open platform, Yuyin Zhuanxie
+
+import numpy as np
+import json
+import ast
+
+# input_path = '/Users/xyqiang/Downloads/weblfasr_python3_demo/raw_data.txt'
+# output_path = '/Users/xyqiang/Downloads/weblfasr_python3_demo/result.txt'
+input_path = 'your_file_path_of_input'
+output_path = 'your_file_path_of_output'
+
+f1= open(input_path)
+lines = f1.readlines()
+all_data_so = []
+for line in lines:
+    item = line.replace("\n", "").split("/getResult success:")[1]
+    item = item.split("'[")[1]
+    item = item.split("]'")[0]
+    # print(item[:16])
+    # print(item[-17:])
+    # print(type(item))
+    user_dicts = ast.literal_eval(item)
+    for user_dict in user_dicts:
+        all_data_so.append(user_dict['onebest'])
+
+# print(len(all_data_so))
+# item = all_data_so[1]
+# print(item)
+with open(output_path, 'w') as f2:
+    f2.writelines(all_data_so)
+
+f1.close()
+f2.close()
